189d4cb789a78dd6dabadeeb6bd0759c64b9808c
DSkirvinC <dskirvinc@gmail.com>
2020-04-01 01:09:11 -0400

    Added Outcome type to Types. close #14

--- Types.hs
+++ Types.hs
@@ -1,3 +1,4 @@
 module Types where
    data Ground = Mine | Safe deriving (Eq, Show)
    data State = Blank | Flagged | Questioned | Revealed Int deriving (Eq, Show)
+   data Outcome = Win | Loss | Continue
