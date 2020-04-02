b2d051c6bf3d238a396c41845a5a2dbfb7499b88
Paxton Caldwell <caldwellp20@hanover.edu>
2020-04-02 16:37:53 +0000

    Created act and action types, close #11

--- Types.hs
+++ Types.hs
@@ -2,3 +2,6 @@ module Types where
    data Ground = Mine | Safe deriving (Eq, Show)
    data State = Blank | Flagged | Questioned | Revealed Int deriving (Eq, Show)
    data Outcome = Win | Loss | Continue
+   data Act = Reveal | Flag | Question deriving (Eq, Show)
+   type Action = (Int, Int, Act)
+
